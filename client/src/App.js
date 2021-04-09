aef4ddad23918d8239b3fcdd78fff02bb391fc13
Jake Luecke <jrluecke95@gmail.com>
2021-04-09 16:49:16 -0400

    got register page running

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,54 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home'
-import './App.css';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import { BrowserRouter as Router, Route, Switch, NavLink } from "react-router-dom";
+import Home from "./pages/Home";
+import "./App.css";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <NavLink to='/'>Home</NavLink>
+              <NavLink to="/register">Register</NavLink>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item to="/">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
         <Switch>
-          <Route path='/' exact>
+          <Route path="/" exact>
             <Home />
           </Route>
-          <Route path='/login'>
+          <Route path="/login">
             <Login />
           </Route>
-          <Route path='/register'>
+          <Route path="/register">
             <Register />
           </Route>
         </Switch>
